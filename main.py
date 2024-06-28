bff645a59958773b77e06da964850e983e74b0e7
jxia3 <97870693+jxia3@users.noreply.github.com>
2024-06-27 19:30:47 -0700

    add ppo file

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from algos import discrete_actor_critic
+from algos import discrete_ppo
 from algos import discrete_reinforce
 from loggers.chart_logger import ChartLogger
-
-logger = ChartLogger(("reward", "actor_loss", "critic_loss"))
-discrete_actor_critic.train("space_invaders", 0, logger, verbose=True)
-logger.plot_metric("reward")
-logger.plot_metric("actor_loss")
-logger.plot_metric("critic_loss")
 
 def run_on_cpu():
     import jax
     jax.config.update("jax_platform_name", "cpu")
+
+run_on_cpu()
+
+logger = ChartLogger(("reward", "actor_loss", "critic_loss"))
+discrete_ppo.train("cartpole", 0, logger, verbose=True)
+logger.plot_metric("reward")
+logger.plot_metric("actor_loss")
+logger.plot_metric("critic_loss")
