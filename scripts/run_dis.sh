d5249264763c2c5f8a46e365be04be8e9f1a94e6
Arnie-He <arniehe2004@gmail.com>
2025-01-27 18:29:41 -0500

    wtf

--- scripts/run_dis.sh
+++ scripts/run_dis.sh
@@ -10,25 +10,25 @@ for seed in "${seeds[@]}"; do
     env_name="cartpole"
     echo "Running task=$env_name with seed=$seed"
     # Nystrom1
-    python main.py --task=$env_name --nested=5 --steps=5e5 --seed=$seed
+    python main.py --task=$env_name --algo=nystrom --nested=10 --steps=5e5 --seed=$seed --ihvp=1.4 --clipf=1.0
     # Nested
-    python main.py --task=$env_name --nested=5 --steps=5e5 --vanilla=True --seed=$seed
+    python main.py --task=$env_name --algo=nested --nested=10 --steps=5e5 --seed=$seed
     # CG
-    python main.py --task=$env_name --nested=5 --steps=5e5 --algo=cg --seed=$seed
+    python main.py --task=$env_name --algo=cg --nested=10 --steps=5e5 --seed=$seed --ihvp=1.4 --clipf=1.0
     # Vanilla
-    python main.py --task=$env_name --steps=5e5 --algo=ppo --seed=$seed
+    python main.py --task=$env_name --algo=ppo --steps=5e5 --seed=$seed
 done
 
 
-# for seed in "${seeds[@]}"; do
-#     env_name="acrobot"
-#     echo "Running task=$env_name with seed=$seed"
-#     # Nystrom1
-#     python main.py --task=$env_name --nested=5 --steps=4e5 --seed=$seed
-#     # Nested
-#     python main.py --task=$env_name --nested=5 --steps=4e5 --vanilla=True --seed=$seed
-#     # CG
-#     python main.py --task=$env_name --nested=5 --steps=4e5 --algo=cg --seed=$seed
-#     # Vanilla
-#     python main.py --task=$env_name --steps=4e5 --algo=ppo --seed=$seed
-# done
+for seed in "${seeds[@]}"; do
+    env_name="acrobot"
+    echo "Running task=$env_name with seed=$seed"
+    # Nystrom1
+    python main.py --task=$env_name --algo=nystrom --nested=10 --steps=5e5 --seed=$seed --ihvp=2 --clipf=1.16662 
+    # Nested
+    python main.py --task=$env_name --algo=nested --nested=10 --steps=5e5 --seed=$seed
+    # CG
+    python main.py --task=$env_name --algo=cg --nested=10 --steps=5e5 --seed=$seed --ihvp=2 --clipf=1.16662 
+    # Vanilla
+    python main.py --task=$env_name --algo=ppo --steps=5e5 --seed=$seed
+done
