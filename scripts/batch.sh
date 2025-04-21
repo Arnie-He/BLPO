f89e3a0395b9944679cf1c6b9db60974d129f20d
Arnie-He <arniehe2004@gmail.com>
2025-04-21 09:29:16 -0400

    updated gitignore

--- scripts/batch.sh
+++ scripts/batch.sh
@@ -4,7 +4,7 @@
 #SBATCH -N 1
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=8:30:00
+#SBATCH --time=16:00:00
 #SBATCH --mem=64GB
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
@@ -13,7 +13,7 @@
 
 module load python/3.11
 export PYTHONPATH=$(pwd):$PYTHONPATH
-source venv10/bin/activate
+source .venv/bin/activate
 
 python main_brax.py --task=walker2d --algo=nystrom
 python main_brax.py --task=walker2d --algo=ppo
